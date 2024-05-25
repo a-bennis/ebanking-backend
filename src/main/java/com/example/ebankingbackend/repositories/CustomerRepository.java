4da994ec90c61f4c69e08c972866293f0547441f
Abdou BENNIS <abdoubennis12@gmail.com>
2024-05-25 16:33:18 +0100

    commit

--- src/main/java/com/example/ebankingbackend/repositories/CustomerRepository.java
+++ src/main/java/com/example/ebankingbackend/repositories/CustomerRepository.java
@@ -1,7 +1,7 @@
 package com.example.ebankingbackend.repositories;
 
 import com.example.ebankingbackend.entities.Customer;
-import org.sid.ebankingbackend.entities.Customer;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
