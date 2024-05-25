5571b58e6e32c45d0ad077fc675d11dd3a40c62a
Abdou BENNIS <abdoubennis12@gmail.com>
2024-05-25 16:32:47 +0100

    Initial commit

---
+++ src/main/java/com/example/ebankingbackend/dtos/CustomerDTO.java
@@ -0,0 +1,17 @@
+package com.example.ebankingbackend.dtos;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+import java.util.List;
+
+@Data
+public class CustomerDTO {
+    private Long id;
+    private String name;
+    private String email;
+}
---
+++ src/main/java/com/example/ebankingbackend/dtos/DebitDTO.java
@@ -0,0 +1,9 @@
+package com.example.ebankingbackend.dtos;
+import lombok.Data;
+
+@Data
+public class DebitDTO {
+    private String accountId;
+    private double amount;
+    private String description;
+}
